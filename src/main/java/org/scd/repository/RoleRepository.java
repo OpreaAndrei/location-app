4a114fe64888fd4a0cf4181d0c8f632caa4a2d6c
Oprea Andrei <andreirobert9898@gmail.com>
2021-03-02 13:37:51 +0200

    Revert "My frist Commit"
    This reverts commit e63b9332f8bad797ec4695ee499d77455667a3ed.

--- src/main/java/org/scd/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package org.scd.repository;
-
-import java.util.Set;
-import org.scd.model.security.Role;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-    Set<Role> findByRole(String role);
-}
